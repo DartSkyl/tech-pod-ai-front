a95aef5a8b3c8e93cfaf1d5033a481b9e5135118
Инна Клементьева <iklementeva@itinkers.com.ua>
2024-08-03 00:07:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from 'vue';
+import IconClose from '@/components/Icons/IconClose.vue';
+
+const collapsed = ref(true)
+
+const sendMessageDisabled = ref(false)
+
+const muted = ref(false)
+
+// todo
+
+</script>
+
+<template>
+  <div class="ricochat">
+    <div v-if="collapsed" class="chat-collapsed">
+      <div class="chat-greeting">Hello! Can I help you?</div>
+      <div class="chat-notifications">2</div>
+      <button v-show="collapsed" @click="collapsed = false" class="chat-entry" aria-label="Open chat"></button>
+    </div>
+
+    <div v-else class="chat-conversation">
+      <div class="chat-close">
+        <button @click="collapsed = true" aria-label="Collapse chat">
+          <IconClose/>
+        </button>
+      </div>
+
+      <div class="chat-body">
+        <div class="header operator">
+          <div class="header__left">
+            <div class="operator__name">Bot Name</div>
+            <div class="operator__status">Online</div>
+            <button class="sound" :aria-label="muted ? 'Enable sounds' : 'Mute sounds'"></button>
+          </div>
+          <div class="header__right">
+            <div class="avatar">
+              <img src="" alt="">
+              <span class="avatar__status"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="main">
+
+        </div>
+
+        <div class="input">
+          <textarea class="input__field" placeholder="Type your message..."></textarea>
+          <div class="input__buttons">
+            <button aria-label="Send a message" :disabled="sendMessageDisabled"></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.ricochat {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  padding: 40px;
+  width: 100%;
+  height: 100%;
+  max-width: 478px;
+  max-height: 738px;
+  min-height: 0;
+  min-width: 0;
+  overflow: hidden;
+  z-index: 2147483639;
+}
+
+</style>
---
+++ src/components/Icons/IconClose.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg class="icon" viewBox="0 0 16 16" aria-hidden="true">
+    <path d="M1 1L15 15" stroke="currentColor" stroke-linecap="round"/>
+    <path d="M15 1L1 15" stroke="currentColor" stroke-linecap="round"/>
+  </svg>
+</template>
